5c797496996e56d80418b25cb07bc9197df74bf3
Trimbachc1153 <101837198+Trimbachc1153@users.noreply.github.com>
2022-03-28 09:34:58 +1300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-hello
+valid = False
+while not valid:
+
+    response = float(input("enter a number"))
+
+    if response > 0:
+         valid = True
+
+    else:
+        print("Please enter a valid number")
+        print()
