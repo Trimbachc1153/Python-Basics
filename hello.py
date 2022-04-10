c10842579efc2b1f0f37c9c7bd64aad2c6950ec1
Trimbachc1153 <101837198+Trimbachc1153@users.noreply.github.com>
2022-04-11 09:41:16 +1200

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
-print("hello")
+# checks that users enter a number that is greater than zero
+valid = False
+while not valid:
+
+    error = "Please enter a number more than zero"
+
+    try:
+    
+        # ask useer to enter a number
+        response = float(input("Enter a number"))
+
+        # checks number is more than zero
+        if response > 0:
+            valid = True
+
+        #outputs error if input is invalid
+        else:
+            print("Please enter a number more than zero")
+            print()
+
+    except ValueError:
+        print(error)
